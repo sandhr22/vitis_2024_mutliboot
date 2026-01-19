8d5edbd7612545e02e60a8b75ad1a97477da9220
Rajveer <sandhr22@mcmaster.ca>
2026-01-18 21:11:29 -0500

    initial split workspace image

---
+++ zynq_fsbl_bsp/libsrc/build_configs/gen_bsp/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,19 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "")
+  file(REMOVE_RECURSE
+  "C:\\ArtyZ7\\artyz7_intro_proj\\split_workspace\\artyz7_intro_platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\diskio.h"
+  "C:\\ArtyZ7\\artyz7_intro_proj\\split_workspace\\artyz7_intro_platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\ff.h"
+  "C:\\ArtyZ7\\artyz7_intro_proj\\split_workspace\\artyz7_intro_platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\ffconf.h"
+  "C:\\ArtyZ7\\artyz7_intro_proj\\split_workspace\\artyz7_intro_platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\sleep.h"
+  "C:\\ArtyZ7\\artyz7_intro_proj\\split_workspace\\artyz7_intro_platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xilffs.h"
+  "C:\\ArtyZ7\\artyz7_intro_proj\\split_workspace\\artyz7_intro_platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xilffs_config.h"
+  "C:\\ArtyZ7\\artyz7_intro_proj\\split_workspace\\artyz7_intro_platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xilrsa.h"
+  "C:\\ArtyZ7\\artyz7_intro_proj\\split_workspace\\artyz7_intro_platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xiltimer.h"
+  "C:\\ArtyZ7\\artyz7_intro_proj\\split_workspace\\artyz7_intro_platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xtimer_config.h"
+  "C:\\ArtyZ7\\artyz7_intro_proj\\split_workspace\\artyz7_intro_platform\\zynq_fsbl\\zynq_fsbl_bsp\\lib\\libxilffs.a"
+  "C:\\ArtyZ7\\artyz7_intro_proj\\split_workspace\\artyz7_intro_platform\\zynq_fsbl\\zynq_fsbl_bsp\\lib\\libxilrsa.a"
+  "C:\\ArtyZ7\\artyz7_intro_proj\\split_workspace\\artyz7_intro_platform\\zynq_fsbl\\zynq_fsbl_bsp\\lib\\libxiltimer.a"
+  )
+endif()
